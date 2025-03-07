98000823337b86187c2e89d5f58a9f237f0c687b
Giliard Godoi <giliard.godoi@gmail.com>
2025-03-07 19:35:37 -0300

    improve button "scroll to the top" behaviour

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     var btnTopo = document.getElementById("btnTopo");
 
+    let lastScroll = window.scrollY;
+    let threshold = 10; // Mínima diferença para considerar movimento
     // Mostra o botão quando o usuário rolar 300px para baixo
     window.addEventListener("scroll", function () {
-        if (window.scrollY > 450) {
+        let currentScroll = window.scrollY;
+        if (lastScroll - currentScroll > threshold) {
             btnTopo.classList.add("show"); // Mostra suavemente
-        } else {
+        }
+        else if (currentScroll - lastScroll > threshold || currentScroll === 0) {
             btnTopo.classList.remove("show"); // Esconde suavemente
         }
+        lastScroll = currentScroll;
     });
 
     // Faz a página rolar suavemente para o topo ao clicar
